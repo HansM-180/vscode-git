2116d9481940e7aa3babddd0a081c7685816d881
Hans <mhansanthony@gmail.com>
2025-09-03 15:47:36 +0800

    initial commit

---
+++ helloworld.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age = 18;
+    bool student = false;
+    std::string car = "Ford";
+
+    cout << "Hello, World! I'm Hans and I will become a Programmer! lol.\n";
+    cout << age << " I'm a " << student << '\n';
+    cout << "I drive a " << car << " LOL" << '\n';
+
+    return 0;
+}
+
+
+
+
