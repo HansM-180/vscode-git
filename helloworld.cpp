52fee912bc78460723e5c45a360ad6f04b9a618c
Hans <mhansanthony@gmail.com>
2025-09-03 23:09:19 +0800

    added temperature

--- helloworld.cpp
+++ helloworld.cpp
@@ -7,6 +7,7 @@ int main()
     int age = 18;
     bool student = false;
     std::string car = "Ford";
+    double temperature = 26.5;
 
     cout << "Hello, World! I'm Hans and I will become a Programmer! lol.\n";
     cout << age << " I'm a " << student << '\n';
